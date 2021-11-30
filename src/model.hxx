5c89817ed4c199149f5ecc56b17a97c0a268fada
eton1 <ethanchan2024@u.northwestern.edu>
2021-11-30 12:12:40 -0600

    actual flappy

--- src/model.hxx
+++ src/model.hxx
@@ -4,7 +4,6 @@
 #include "game_config.hxx"
 
 
-
 class Model
 {
 
--- src/view.hxx
+++ src/view.hxx
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "model.hxx"
+#include <ge211.hxx>
 
 class View
 {
@@ -8,7 +9,9 @@ public:
     explicit View(Model const& model);
 
     void draw(ge211::Sprite_set& set);
-
+    ge211::Dims<int> initial_window_dimensions() const;
 private:
     Model const& model_;
+    ge211::Circle_sprite const bird_sprite;
+    ge211::Rectangle_sprite const col_sprite;
 };
